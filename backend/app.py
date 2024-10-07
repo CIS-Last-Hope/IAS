da4eb7c76692f3c8fe0609ef2bd93215895b6edf
Tomsh666 <tomshinskiy02@gmail.com>
2024-10-07 11:54:48 +0300

    chore: deploy fix 1

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://example.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
